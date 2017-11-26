5750773eaf9dec30466cbbc8ffa198bbce133d15
Todd Pinkerton <todd.pinkerton@gmail.com>
2017-11-26 14:33:03 -0500

    markup and components for VideoList and VideoListItem.

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,12 +1,6 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
-  /* render()  {
-    //return <input onChange={this.onInputChange} />;
-    //return <input onChange={(event) => console.log(event.target.value)} />;
-
-  }
-*/
 
 constructor(props) {
   super(props);
@@ -17,17 +11,10 @@ constructor(props) {
 render()  {
   return (
     <div>
-   <input onChange={(event) => this.setState({term: event.target.value})} />;
-   Value of the input: {this.state.term}
+   <input onChange={(event) => this.setState({term: event.target.value})} />
    </div>
    );
 }
-
-/*
-  onInputChange(event) {
-    console.log(event.target.value)
-  }
-*/
 
 }
 export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,24 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
-
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY='AIzaSyBnHNyZtHtozKjfE02EhVmF8F3TdLeSupo'
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
-    console.log(data);
-});
+
 
 //Create a new component. Should produce some HTML.
 
-const App = function() {
-//  return <div>Hi!</div>;
-return (
-  <div>
-  <SearchBar />
-  </div>
-)
-}
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {videos: []};
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+        //console.log(data);
+        this.setState({videos});
+    });
+  }
 
+
+  render() {
+    return (
+      <div>
+      <SearchBar />
+      <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
+}
 //Take this component's generated HTML and put it in the page / DOM.
 ReactDOM.render(< App />, document.querySelector('.container'));
